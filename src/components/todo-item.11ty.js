07fa2e4eb81c27255bec3b5da1132bfb194ee652
wommy <tom@technomad.media>
2021-07-29 18:38:40 +0000

    wip

--- src/components/todo-item.11ty.js
+++ src/components/todo-item.11ty.js
@@ -1,5 +1,5 @@
 module.exports = todo => `
-	<li class="my-4">
+	<li>
 		<a${ todo.link ? ' href="'+ todo.link + '" target="blank"' : '' }>${ todo.name }</a>
 		<div>${ todo.category }</div>
 	</li>
