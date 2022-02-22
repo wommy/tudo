9b58b5f37ebba4930d73b6e8ec7ffed658c2948f
wommy <tom@technomad.media>
2022-02-22 01:01:13 -0500

    functions

---
+++ src/functions/bookmark.js
@@ -0,0 +1,38 @@
+import client from '../utils/sanityClient'
+
+const handler = async event => {
+  if (!event.httpMethod === 'POST') {
+    return {
+      statusCode: 400,
+      body: 'unrecognized HTTP Method, only POST allowed',
+    }
+  }
+
+  const { url, title } = Object.fromEntries(
+    event.rawQuery.split('&').map(x => x.split('=')),
+  )
+
+  const document = {
+    _type: 'bookmark',
+    category: ['inbox'],
+    title: decodeURIComponent(title),
+    url: decodeURIComponent(url),
+  }
+
+  try {
+    const result = await client.create(document)
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(result),
+    }
+  } catch (error) {
+    return {
+      headers: { 'Content-Type': 'application/json' },
+      statusCode: 500,
+      body: error.responseBody || JSON.stringify({ error: 'An error occurred' }),
+    }
+  }
+}
+
+module.exports = { handler }
---
+++ src/functions/bookmark2.js
@@ -0,0 +1,21 @@
+const input =
+  '?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DF6Wfy9Ro4s4&title=How%20to%20be%20an%20actual%20god%20on%20Pudge%20-%20YouTube'
+    .split('?')[1]
+    .split('&')
+    .map(x => x.split('='))
+
+const { url, title } = Object.fromEntries(input)
+
+console.log(decodeURIComponent(url))
+console.log(decodeURIComponent(title))
+
+// let { url, title } = Object.fromEntries(event.body.split("&").map( x =>  x.split("=") ))
+
+// const document = {
+//   _type: 'bookmark',
+//   category: ['inbox'],
+//   title: decodeURIComponent( title ),
+//   url: decodeURIComponent( url ),
+// }
+
+// console.log(document)
