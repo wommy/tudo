8d04f041670edb2ded79ae4bf4186b923e14308b
wommy <tom@technomad.media>
2022-02-24 18:55:01 -0500

    move components into main for performance

--- src/Tudo.tsx
+++ src/Tudo.tsx
@@ -1,12 +1,39 @@
-import type { Component } from 'solid-js'
-import { Form, Header, TudoList } from './components'
+import { Component, createResource, For, Show, Suspense } from 'solid-js'
+import sanityFetch from './utils/sanityFetch'
 
 const Tudo: Component = () => {
+  const [tudos] = createResource(async () => await sanityFetch)
   return (
     <main class='grid place-content-center gap-8 mt-8'>
-      <Header />
-      <Form />
-      <TudoList />
+      
+      {/* simple header */}
+      <header class='text-center'>
+        <h1>tudo</h1>
+      </header>
+
+			{/* form */}
+      <form
+        action='https://todo-tm.netlify.app/api/todo/'
+        method='post'
+        class='flex gap-1'
+      >
+        <label>
+          <input name='todo' placeholder='todo' required />
+        </label>
+
+        <button type='submit'>Submit</button>
+      </form>
+
+      {/* generate List && Items */}
+      <ol className='stack' reversed>
+        <Suspense fallback={<p>loadin..</p>}>
+          <Show when={tudos()}>
+            <For each={tudos().reverse()}>{tudo => 
+              <li>{tudo as string}</li>
+            }</For>
+          </Show>
+        </Suspense>
+      </ol>
     </main>
   )
 }
