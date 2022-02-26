4127f3c836acf9494f02433bb21133a8ea26462e
wommy <tom@technomad.media>
2022-02-26 03:00:09 -0500

    prepping for live

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,8 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_SANITY_PROJECTID: string
   readonly VITE_SANITY_DATASET: string
-  readonly VITE_SANITY_TOKEN: string
   readonly VITE_POST_URL: string
+  readonly SANITY_TOKEN: string
 }
 
 interface ImportMeta {
